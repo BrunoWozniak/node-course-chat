2e162eee0365d72bf770677d39b6de6d722578c7
BrunoWozniak <bruno.wozniak@gmail.com>
2017-09-07 20:58:24 +0200

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+var app = express();
+// const port = process.env.PORT;
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '/../public');
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
+
+module.exports = {app};
