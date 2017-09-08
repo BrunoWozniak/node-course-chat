077013df5fa81750db1073536d6b2c91ef10d1a8
BrunoWozniak <bruno.wozniak@gmail.com>
2017-09-08 20:38:55 +0200

    Wired createMessage and newMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,6 @@ var socket = io();
 
 socket.on('connect', () => {
    console.log('Connected to server');
-   socket.emit('createMessage', {
-      from: 'bruno@client.com',
-      text: 'Message created from client side'
-   });
 });
 
 socket.on('newMessage', function (message) {
