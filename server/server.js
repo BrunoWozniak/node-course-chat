68180fb519b09ec2aca3363c32b607b49084b655
BrunoWozniak <bruno.wozniak@gmail.com>
2017-09-11 22:10:30 +0200

    Added sendMyLocation, added jQuery, added CSS

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage, generateLocationMessage} = require('./utils/message');
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '/../public');
-const {generateMessage} = require('./utils/message');
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
@@ -13,29 +13,27 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-   console.log('New user connected');
+  console.log('New user connected');
 
-   socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-   socket.broadcast.emit('newMessage', generateMessage('Admin', 'A new user has joined'));
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-   socket.on('createMessage', (message) => {
-      console.log('createMessage', message);
-      // io.emit('newMessage', {
-      //    from: message.from,
-      //    text: message.text,
-      //    createAt: new Date().getTime()
-      // });
-      socket.broadcast.emit('newMessage', generateMessage(message.from, message.text));
-   });
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback();
+  });
 
-   socket.on('disconnect', () => {
-      console.log('User disconnected');
-   });
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
 });
 
 server.listen(port, () => {
-  console.log(`Started up at port ${port}`);
+  console.log(`Server is up on ${port}`);
 });
-
-module.exports = {app};
