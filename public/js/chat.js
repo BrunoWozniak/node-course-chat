41fd557523d4d42862314ebc1965b891bcf6b907
BrunoWozniak <bruno.wozniak@gmail.com>
2017-09-14 21:19:37 +0200

    Added users and rooms

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,49 +1,68 @@
 var socket = io();
 
 function scrollToBottom () {
-   // Selectors
-   var messages = jQuery('#messages');
-   var newMessage = messages.children('li:last-child');
-   // Heights
-   var clientHeight = messages.prop('clientHeight');
-   var scrollTop = messages.prop('scrollTop');
-   var scrollHeight = messages.prop('scrollHeight');
-   var newMessageHeight = newMessage.innerHeight();
-   var lastMessageHeight = newMessage.prev().innerHeight();
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child')
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
 
-   if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
-      messages.scrollTop(scrollHeight);
-   }
-};
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
 
 socket.on('connect', function () {
-  console.log('Connected to server');
+  var params = jQuery.deparam(window.location.search);
+
+  socket.emit('join', params, function (err) {
+    if (err) {
+      alert(err);
+      window.location.href = '/';
+    } else {
+      console.log('No error');
+    }
+  });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function (message) {
-   var formattedTime = moment(message.createdAt).format('h:mm a');
-   var template = jQuery('#message-template').html();
-   var html = Mustache.render(template, {
-      text: message.text,
-      from: message.from,
-      createdAt: formattedTime
-   });
+socket.on('updateUserList', function (users) {
+  var ol = jQuery('<ol></ol>');
 
-   jQuery('#messages').append(html);
-   scrollToBottom();
+  users.forEach(function (user) {
+    ol.append(jQuery('<li></li>').text(user));
+  });
+
+  jQuery('#users').html(ol);
+});
+
+socket.on('newMessage', function (message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
   var template = jQuery('#location-message-template').html();
   var html = Mustache.render(template, {
-     url: message.url,
-     from: message.from,
-     createdAt: formattedTime
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
   });
 
   jQuery('#messages').append(html);
@@ -56,7 +75,6 @@ jQuery('#message-form').on('submit', function (e) {
   var messageTextbox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextbox.val()
   }, function () {
     messageTextbox.val('')
