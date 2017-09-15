8a8606fb61ba947edddfd96ef61789c8b2da157a
BrunoWozniak <bruno.wozniak@gmail.com>
2017-09-15 23:13:14 +0200

    Made room non-case-sensitive

--- server/server.js
+++ server/server.js
@@ -19,6 +19,8 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  params.room = params.room.toUpperCase();
+
   socket.on('join', (params, callback) => {
     if (!isRealString(params.name) || !isRealString(params.room)) {
       return callback('Name and room name are required.');
@@ -48,7 +50,7 @@ io.on('connection', (socket) => {
     var user = users.getUser(socket.id);
 
     if (user) {
-      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));  
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
     }
   });
 
